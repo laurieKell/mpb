6eb8e8532d78a18425712833cd2132a5bd52a355
iagomosqueira <iago@mosqueira.eu>
2016-04-26 15:13:26 +0200

    Rcpp code moved to Rcpp branch

--- inst/tpl/backup/ll-stripped.cpp
+++
@@ -1,9 +0,0 @@
-FUNCTION get_neglogL
-
-  neglogL = halfnlog2pi;
-  for (int j=1; j<=ni; j++)
-     neglogL += log(s_)+  
-                pow(log(I[j])-log(Ifit[j]),2.0)/(2*_s[Idx[j]]*s_);
-     
-
-
--- inst/tpl/dnorm.hpp
+++
@@ -1,4 +0,0 @@
-#include <admodel.h>
-dvariable dnorm(const double&, const dvariable&, const dvariable&);
-dvariable dnorm(prevariable&, double&, double&);
-dvariable dnorm(const dvector&, const dvar_vector&, const dvar_vector&);
