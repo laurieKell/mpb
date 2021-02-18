50ae1dc525088919e17b0e5f8f3efedc21e9e758
lauriekell <laurie@kell.es>
2021-02-18 16:34:17 +0000

    changed output from data.frame to model.frame

--- R/getBiodyn.R
+++ R/getBiodyn.R
@@ -34,20 +34,40 @@ getOM<-function(x,what="FLStock"){
 
 bdTimeSeries<-function(om,eq){
   FLQuants(om,"Stock"  =function(x) ssb(x)[,,"F"]/refpts(eq)["msy","ssb"],
+              "Blim"   =function(x) ssb(x)[,,"F"]/refpts(eq)["virgin","ssb"],
               "Harvest"=function(x) apply(catch(x)/stock(x),2,mean)/(refpts(eq)["msy","yield"]/refpts(eq)["msy","biomass"]),
               "Catch"  =function(x) 0.5*apply(catch(x),2,sum)/refpts(eq)["msy","yield"])}
 
 refTimeSeries<-function(om,eq,mp){
-  om=as.data.frame(bdTimeSeries(om,eq),drop=TRUE)
+  om=model.frame(bdTimeSeries(om,eq),drop=TRUE)
 
-  mp=as.data.frame(FLQuants(mp,"Stock"  =function(x) stock(  x)/refpts(x)["bmsy"],
-                               "Harvest"=function(x) harvest(x)/refpts(x)["fmsy"],
-                               "Catch"  =function(x) catch(  x)/refpts(x)["msy"]),drop=TRUE)
+  mp=model.frame(mcf(FLQuants(mp,"Stock"  =function(x) stock(  x)/refpts(x)["bmsy"],
+                                 "Blim"   =function(x) stock(  x)/params(x)["k"],
+                                 "Harvest"=function(x) harvest(x)/refpts(x)["fmsy"],
+                                 "Catch"  =function(x) catch(  x)/refpts(x)["msy"])),drop=TRUE)
 
   rtn=rbind.fill(cbind(what="OM",om),
                  cbind(what="MP",mp))
   
   rtn}
+
+smryBiodyns<-function(jb,om,eq){
+  mp=jabba2biodyn(jb)
+  rf=c(refpts(mp),params(mp)[c("k","r")])
+  names(rf)=c("msy","fmsy","bmsy","virgin","r")
+  rf=t(as.data.frame(rf))
+  
+  rf2=c(refpts(eq)["msy",c("yield","ssb","biomass","harvest")],refpts(eq)["virgin",c("ssb","biomass")])
+  names(rf2)=c("msy","ssbmsy","bmsy","fmsy","virginssb","virgin")
+  rf2=t(as.data.frame(rf2))
+  
+  rbind.fill(
+    cbind(cbind("what"="OM", model.frame(bdTimeSeries(om, eq),drop=TRUE)),rf2),
+    cbind(mutate(model.frame(FLQuants(mp,
+                                      "Blim"   =function(x) stock(  x)/params(x)["k"],
+                                      "Stock"  =function(x) stock(  x)/refpts(x)["bmsy"],
+                                      "Harvest"=function(x) harvest(x)/refpts(x)["fmsy"],
+                                      "Catch"  =function(x) catch(x)/refpts(x)["msy"]),drop=T)),rf,what="JABBA"))}
 
 if (FALSE){
   dirMy="/home/laurence-kell/Desktop/papers/albio/inputs/ss"
@@ -71,17 +91,17 @@ if (FALSE){
   om =getOM(file.path(dirMy,run,"om.RData"))
   eq =getOM(file.path(dirMy,run,"om.RData"),"FLBRP")
   dat=rbind.fill(
-        cbind("What"="OM", as.data.frame(bdTimeSeries(om, eq),drop=TRUE)),
-        cbind("What"="MSE",as.data.frame(bdTimeSeries(mse,eq),drop=TRUE)),
+        cbind("what"="OM", as.data.frame(bdTimeSeries(om, eq),drop=TRUE)),
+        cbind("what"="MSE",as.data.frame(bdTimeSeries(mse,eq),drop=TRUE)),
         mutate(ldply(bds,function(x){ as.data.frame(FLQuants(x,
                                           "Stock"  =function(x) stock(  x)/refpts(x)["bmsy"],
                                           "Harvest"=function(x) harvest(x)/refpts(x)["fmsy"],
                                           "Catch"  =function(x) catch(  x)/refpts(x)["msy"]),drop=T)}),
-                            What=paste("MP",seq(1999,2011,3))[as.numeric(.id)]))[,-5]
+                            what=paste("MP",seq(1999,2011,3))[as.numeric(.id)]))[,-5]
      
   ggplot(dat)+
     geom_hline(aes(yintercept=1),col="red")+
-    geom_line(aes(year,data,col=What))+
+    geom_line(aes(year,data,col=what))+
     facet_grid(qname~.,scale="free")
 }
 
@@ -144,36 +164,20 @@ if (FALSE){
     rf2=t(as.data.frame(rf2))
     
     dat=rbind.fill(
-      cbind(cbind("What"="OM", model.frame(bdTimeSeries(om, eq),drop=TRUE)),rf2),
+      cbind(cbind("what"="OM", model.frame(bdTimeSeries(om, eq),drop=TRUE)),rf2),
       cbind(mutate(model.frame(FLQuants(mp,
                                 "Stock"  =function(x) stock(  x)/refpts(x)["bmsy"],
                                 "Harvest"=function(x) harvest(x)/refpts(x)["fmsy"],
                                 "Catch"  =function(x) catch(x)/refpts(x)["msy"]),drop=T)),rf,
-            What="JABBA"))
+            what="JABBA"))
     dat})
 
-  ggplot(melt(dat[,1:6],id=c("X1","What","year")))+
+  ggplot(melt(dat[,1:6],id=c("X1","what","year")))+
     geom_hline(aes(yintercept=1),col="red")+
-    geom_line(aes(year,value,col=What,group=paste(What,X1)))+
+    geom_line(aes(year,value,col=what,group=paste(what,X1)))+
     facet_grid(variable~.,scale="free")
   }
 
-smryBiodyns<-function(jb,om,eq){
-  mp=jabba2biodyn(jb)
-  rf=c(refpts(mp),params(mp)[c("k","r")])
-  names(rf)=c("msy","fmsy","bmsy","virgin","r")
-  rf=t(as.data.frame(rf))
-  
-  rf2=c(refpts(eq)["msy",c("yield","ssb","biomass","harvest")],refpts(eq)["virgin",c("ssb","biomass")])
-  names(rf2)=c("msy","ssbmsy","bmsy","fmsy","virginssb","virgin")
-  rf2=t(as.data.frame(rf2))
-  
-  rbind.fill(
-    cbind(cbind("What"="OM", model.frame(bdTimeSeries(om, eq),drop=TRUE)),rf2),
-    cbind(mutate(model.frame(FLQuants(mp,
-                                      "Stock"  =function(x) stock(  x)/refpts(x)["bmsy"],
-                                      "Harvest"=function(x) harvest(x)/refpts(x)["fmsy"],
-                                      "Catch"  =function(x) catch(x)/refpts(x)["msy"]),drop=T)),rf,What="JABBA"))}
 
 if (FALSE){
   
